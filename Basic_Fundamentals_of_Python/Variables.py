46accacae1c52f12670bafd81a4f65da0feb2787
thrinethra <ms3nethra@gmail.com>
2024-05-18 15:00:15 +0530

    print hello world and variables

---
+++ Basic_Fundamentals_of_Python/Variables.py
@@ -0,0 +1,25 @@
+#A variable name must start with a letter or the underscore character
+
+a = 5
+print(a)
+
+apple = 123
+print(apple)
+
+#A variable name cannot start with a number
+#3nethra = 123
+
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+rahul_12 = 45
+print(rahul_12)
+
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
+book = 1
+Book = 1
+BOOK = 1
+print(book)
+print(Book)
+print(BOOK)
+
+#A variable name cannot be any of the Python keywords.
+help("keywords")
