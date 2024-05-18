d96cc175763afe5153779351c65905c373ff5316
thrinethra <ms3nethra@gmail.com>
2024-05-18 16:35:53 +0530

    python comments, data types

---
+++ Basic_Fundamentals_of_Python/Data_Types.py
@@ -0,0 +1,52 @@
+# Integers
+number = 24
+print(number)
+
+
+#type
+a = 23
+print(type(a))
+print (a)
+
+print("a")
+
+
+# float
+f = 1.0
+print(f)
+print(type(f))
+
+
+
+# Booleans
+b = True
+print(type(b))
+print(b)
+
+b1 = False
+print(type(b1))
+print(b1)
+
+
+# Strings
+s = "Python for beginners 123 @#$"
+print(type(s))
+print(s)
+
+s1 = 'basic fundamentals 123 @#$'
+print(type(s1))
+print(s1)
+
+
+m1 = """
+this is multiline string!
+"""
+print(type(m1))
+print(m1)
+
+
+# None
+n = None;
+print(type(n))
+print(n)
+
