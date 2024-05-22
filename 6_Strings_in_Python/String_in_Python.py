ff08f0ae5373fa9556b2110a80b09dd47eebb40e
thrinethra <ms3nethra@gmail.com>
2024-05-22 19:57:50 +0530

    strings

---
+++ 6_Strings_in_Python/String_in_Python.py
@@ -0,0 +1,12 @@
+""" Strings
+- A string is a sequence of character
+- character can be
+ - Alphabets (capital, small)
+ - digits - 0 - 9
+ - whitespace 
+ - special chatrcters 
+
+they can be defined using single, double, or triple quotes
+
+"""
+
